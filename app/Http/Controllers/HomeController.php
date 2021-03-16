eb0f5e7080aabdae319551e88b95249203062931
auzanzulhazmi <zulhazmiauzan@gmail.com>
2021-03-16 10:37:33 +0700

    ui boostrap --auth

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,17 +2,27 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Post;
-use App\Models\Anggota;
-use App\Models\Menu;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index() {
-        return view('index')
-                ->with('posts', Post::index())
-                ->with('anggotas', Anggota::anggota())
-                ->with('menus', Menu::menu());
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,7 @@ Route::get('/menu', [MenuController::class, 'index']);
 Route::get('/products', [ProductController::class, 'index']);
 
 Route::get('/pegawai', [PegawaiController::class, 'index']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
